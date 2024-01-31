2ca3a4f03184be4b36f89b07c44b0301ced5e7ee
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-31 00:24:30 -0300

    add .env
      flutter_dotenv: ^5.1.0
      crypto: ^3.0.3
      convert: ^3.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:first_proj_flutter_dio/src/imc/imc_hive_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final di = GetIt.asNewInstance();
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   AppModule.start();
   await Hive.initFlutter();
 
