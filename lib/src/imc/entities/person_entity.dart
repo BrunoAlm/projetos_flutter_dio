45aa4081f0bf0ae6d0371b3eb17e6adb4fb421e1
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-30 22:18:47 -0300

    rename entity

--- lib/src/imc/entities/person_entity.dart
+++ lib/src/imc/entities/person_entity.dart
@@ -1,11 +1,11 @@
-class Pessoa {
+class Person {
   final String nome;
   final String altura;
   final String peso;
   final String classificacao;
   final double imc;
 
-  Pessoa({
+  Person({
     required this.nome,
     required this.altura,
     required this.peso,
