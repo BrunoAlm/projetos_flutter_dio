e42262a567131806f8bfa8df7ad8a30c8491fb3a
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-30 22:18:13 -0300

    change folder

--- lib/src/shared/custom_text_form_field.dart
+++ lib/src/shared/custom_text_form_field.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final int? maxLenght;
+  final Function(String)? onChanged;
 
   const CustomTextField({
     super.key,
     required this.controller,
     required this.label,
+    this.onChanged,
+    this.maxLenght,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      onChanged: onChanged,
+      maxLength: maxLenght,
       decoration: InputDecoration(
         label: Text(label),
         enabledBorder: const OutlineInputBorder(),
